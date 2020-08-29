e9b2fa4b1f2f1f155d6876831598f23560f6df34
shaoying <shaoying@zuoyebang.com>
2020-08-29 14:04:51 +0800

    opt some

--- src/gcc/gcc.go
+++ src/gcc/gcc.go
@@ -10,14 +10,33 @@ const GCC = "gcc"
 
 var Index = &ice.Context{Name: GCC, Help: "编译器",
 	Configs: map[string]*ice.Config{
-		GCC: {Name: GCC, Help: "编译器", Value: kit.Data()},
+		GCC: {Name: GCC, Help: "编译器", Value: kit.Data(
+			"source", "http://mirrors.aliyun.com/gnu/gcc/gcc-4.8.5/gcc-4.8.5.tar.gz",
+			"build", []interface{}{
+				"--enable-languages=c",
+			},
+		)},
 	},
 	Commands: map[string]*ice.Command{
 		ice.CTX_INIT: {Hand: func(m *ice.Message, c *ice.Context, cmd string, arg ...string) {}},
 		ice.CTX_EXIT: {Hand: func(m *ice.Message, c *ice.Context, cmd string, arg ...string) {}},
 
-		GCC: {Name: GCC, Help: "编译器", Hand: func(m *ice.Message, c *ice.Context, cmd string, arg ...string) {
-			m.Echo("hello gcc world")
+		GCC: {Name: "gcc port=auto path=auto auto 启动:button 构建:button 下载:button", Help: "命令行", Action: map[string]*ice.Action{
+			"download": {Name: "download", Help: "下载", Hand: func(m *ice.Message, arg ...string) {
+				m.Cmdy(code.INSTALL, "download", m.Conf(GCC, kit.META_SOURCE))
+			}},
+			"build": {Name: "build", Help: "构建", Hand: func(m *ice.Message, arg ...string) {
+				m.Cmdy(code.INSTALL, "build", m.Conf(GCC, kit.META_SOURCE), m.Confv(GCC, "meta.build"))
+			}},
+			"start": {Name: "start", Help: "启动", Hand: func(m *ice.Message, arg ...string) {
+				m.Optionv("prepare", func(p string) []string {
+					m.Option(cli.CMD_DIR, p)
+					return []string{}
+				})
+				m.Cmdy(code.INSTALL, "start", m.Conf(GCC, kit.META_SOURCE), "bin/gcc")
+			}},
+		}, Hand: func(m *ice.Message, c *ice.Context, cmd string, arg ...string) {
+			m.Cmdy(code.INSTALL, path.Base(m.Conf(GCC, kit.META_SOURCE)), arg)
 		}},
 	},
 }
