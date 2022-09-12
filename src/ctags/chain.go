1f880a21f224c247933fb1df817eeb5498f72f2a
shylinux@163.com <shylinux>
2022-09-12 14:14:18 +0800

    opt some

--- src/ctags/chain.go
+++ src/ctags/chain.go
@@ -8,7 +8,6 @@ import (
 	"shylinux.com/x/icebergs/base/cli"
 	"shylinux.com/x/icebergs/base/ctx"
 	"shylinux.com/x/icebergs/base/nfs"
-	"shylinux.com/x/icebergs/core/code"
 	"shylinux.com/x/icebergs/core/wiki"
 	kit "shylinux.com/x/toolkits"
 )
@@ -18,21 +17,20 @@ type chain struct {
 	list string `name:"list text auto" help:"框架图"`
 }
 
-func (s chain) processInner(m *ice.Message, arg ...ice.Any) {
-	m.ProcessStory(append(kit.List(m.Prefix(code.INNER)), arg...)...)
+func (s chain) processInner(m *ice.Message, arg ...string) {
+	ctx.ProcessFloat(m.Message, kit.Simple(ice.GetTypeKey(inner{}), arg)...)
 }
 func (s chain) Find(m *ice.Message, arg ...string) {
 	if nfs.ExistsFile(m, path.Join(m.Option(nfs.PATH), arg[0])) {
 		s.processInner(m, m.Option(nfs.PATH), arg[0], "1")
-		return
+		return // 文件
 	}
 
 	if !nfs.ExistsFile(m, path.Join(m.Option(nfs.PATH), nfs.TAGS)) {
 		s.System(m.Spawn(), m.Option(nfs.PATH), "ctags", "-R")
 	}
-
 	ls := kit.Split(arg[0], "", ":=/()")
-	var meta = struct{ name, kind, value, sub, arg string }{name: ls[0]}
+	meta := struct{ name, kind, value, sub, arg string }{name: ls[0]}
 	for i := 1; i < len(ls); i += 2 {
 		switch ls[i] {
 		case ":":
@@ -45,18 +43,16 @@ func (s chain) Find(m *ice.Message, arg ...string) {
 			meta.arg = ls[i+1]
 		}
 	}
-	if msg := m.Cmd(code.INNER, nfs.TAGS, kit.Select(meta.name, meta.kind, meta.value, meta.sub)); msg.Append(nfs.FILE) != "" {
+	if msg := m.Cmd(inner{}, nfs.TAGS, kit.Select(meta.name, meta.kind, meta.value, meta.sub)); msg.Append(nfs.FILE) != "" {
 		s.processInner(m, msg.Append(nfs.PATH), msg.Append(nfs.FILE), msg.Append(nfs.LINE))
-		return
+		return // 索引
 	}
 
-	if msg := s.System(m.Spawn(), "", cli.MAN, "3", arg[0]); cli.IsSuccess(msg) && !strings.HasPrefix(msg.Result(), "No manual entry for") {
-		s.processInner(m, cli.MAN, arg[0], 3)
-		return
-	}
-	if msg := s.System(m.Spawn(), "", cli.MAN, arg[0]); cli.IsSuccess(msg) && !strings.HasPrefix(msg.Result(), "No manual entry for") {
-		s.processInner(m, cli.MAN, arg[0])
-		return
+	for _, n := range []string{"3", "2", ""} {
+		if msg := s.System(m.Spawn(), "", cli.MAN, n, arg[0]); cli.IsSuccess(msg) && !strings.HasPrefix(msg.Result(), "No manual entry for") {
+			s.processInner(m, cli.MAN, arg[0], n)
+			return // 手册
+		}
 	}
 }
 func (s chain) List(m *ice.Message, arg ...string) {
--- src/ctags/inner.go
+++ src/ctags/inner.go
@@ -2,6 +2,7 @@ package ctags
 
 import (
 	"path"
+	"strings"
 
 	"shylinux.com/x/ice"
 	"shylinux.com/x/icebergs/base/cli"
@@ -10,19 +11,34 @@ import (
 
 type inner struct {
 	ice.Code
-	list string `name:"inner path=src/@key file=main.go@key line=1 auto ctags" help:"编辑器"`
+	ctags string `name:"ctags" help:"索引"`
+	list  string `name:"inner path=src/@key file=main.go@key line=1 auto" help:"编辑器"`
 }
 
-func (s inner) Ctags(m *ice.Message, arg ...string) {
-	if !nfs.ExistsFile(m, path.Join(arg[0], nfs.TAGS)) {
-		s.System(m.Spawn(), arg[0], "ctags", "-R")
+func (s inner) Tags(m *ice.Message, arg ...string) {
+	if !nfs.ExistsFile(m, path.Join(m.Option(nfs.PATH), "tags")) {
+		s.System(m.Spawn(), m.Option(nfs.PATH), "ctags", "-a", "-R")
+	}
+	if m.Cmdy("web.code.inner", "tags", arg); m.Length() > 0 {
+		return // 索引
+	}
+	for _, n := range []string{"3", "2", ""} {
+		if msg := s.System(m.Spawn(), "", cli.MAN, n, arg[0]); cli.IsSuccess(msg) && !strings.HasPrefix(msg.Result(), "No manual entry for") {
+			m.Push(nfs.PATH, cli.MAN)
+			m.Push(nfs.FILE, arg[0])
+			m.Push(nfs.LINE, n)
+			return // 手册
+		}
 	}
 }
 func (s inner) Man(m *ice.Message, arg ...string) {
-	m.Cmdy(cli.SYSTEM, "man", arg)
+	m.Cmdy(cli.SYSTEM, cli.MAN, arg)
 }
 func (s inner) List(m *ice.Message, arg ...string) {
 	m.Cmdy("web.code.inner", arg)
+	m.Option("plug", m.Config("show.plug"))
+	m.Option("exts", m.Config("show.exts"))
+	m.Option("tabs", m.Config("show.tabs"))
 }
 
 func init() { ice.CodeCtxCmd(inner{}) }
