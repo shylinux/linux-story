f6b1a5817ea6a3116c1b11ce2256578fe8cee867
shylinux@163.com <shylinux>
2022-09-23 23:41:18 +0800

    opt some

---
+++ iso/alpine/alpine.go
@@ -0,0 +1,39 @@
+package alpine
+
+import (
+	"strings"
+
+	"shylinux.com/x/ice"
+	"shylinux.com/x/icebergs/base/cli"
+	"shylinux.com/x/icebergs/base/nfs"
+	kit "shylinux.com/x/toolkits"
+)
+
+type alpine struct {
+	mirrors string `name:"mirrors from=dl-cdn.alpinelinux.org to=mirrors.tencent.com" help:"镜像"`
+	tzone   string `name:"mirrors tzone=Asia/Shanghai" help:"镜像"`
+	list    string `name:"list auto mirrors" help:"alpine"`
+}
+
+func (s alpine) Mirrors(m *ice.Message, arg ...string) {
+	if !cli.IsAlpine(m.Message) {
+		return
+	}
+	kit.Rewrite("/etc/apk/repositories", func(text string) string {
+		return strings.ReplaceAll(text, m.Option("from"), m.Option("to"))
+	})
+	m.Cmdy(nfs.CAT, "/etc/apk/repositories")
+}
+func (s alpine) Tzone(m *ice.Message, arg ...string) {
+	if !cli.IsAlpine(m.Message) {
+		return
+	}
+	m.Cmd(cli.SYSTEM, "apk", "add", "tzdata")
+	m.Cmd(cli.SYSTEM, "cp", "/usr/share/zoneinfo/"+m.Option("tzone"), "/etc/localtime")
+	m.Cmd(nfs.SAVE, "/etc/timezone", m.Option("tzone"))
+	m.Cmdy(nfs.CAT, "/etc/timezone")
+}
+func (s alpine) List(m *ice.Message, arg ...string) {
+}
+
+func init() { ice.CodeCmd(alpine{}) }
