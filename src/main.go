c954ac89fc243262e78a9d8e4a6b46153d102e96
shy <shy@shylinux.com>
2024-02-24 13:38:44 +0800

    add some

--- src/main.go
+++ src/main.go
@@ -3,6 +3,10 @@ package main
 import (
 	"shylinux.com/x/ice"
 	_ "shylinux.com/x/icebergs/base/aaa/portal"
+	_ "shylinux.com/x/icebergs/core/chat/oauth"
+	_ "shylinux.com/x/icebergs/misc/java"
+	_ "shylinux.com/x/icebergs/misc/node"
+	_ "shylinux.com/x/icebergs/misc/wx"
 
 	_ "shylinux.com/x/linux-story/src/busybox"
 	_ "shylinux.com/x/linux-story/src/gcc"
