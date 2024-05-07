a7c248f891da28bb587aedd42a55e71eb2e75d0a
shy <shy@shylinux.com>
2024-05-07 15:21:31 +0800

    add some

--- src/system/studio.go
+++ src/system/studio.go
@@ -8,11 +8,12 @@ import (
 )
 
 type studio struct {
+	dir  dir
 	list string `name:"list refresh" icon:"linux.png"`
 }
 
 func (s studio) List(m *ice.Message, arg ...string) {
-	if m.Cmdy(dir{}, arg); len(arg) == 0 {
+	if m.Cmdy(dir{}, arg).PushAction(s.dir.Upload, s.dir.Trash).Action(); len(arg) == 0 {
 		m.StatusTimeCount(nfs.VERSION, m.SystemCmdx("uname", "-r")).Display("")
 		kit.If(m.Config(ctx.TOOLS) == "", func() { m.Toolkit(port{}, proc{}, user{}, disk{}) })
 	}
